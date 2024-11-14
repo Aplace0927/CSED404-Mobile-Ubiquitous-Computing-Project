1ec70ad07a69f942660613082c73a95b41a8475a
Aplace <aplace030927@gmail.com>
2024-11-14 18:25:40 +0900

    feat(project): brand new world

---
+++ Projects/app/src/main/java/com/csed433project/hapticfitness/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.csed433project.hapticfitness.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
