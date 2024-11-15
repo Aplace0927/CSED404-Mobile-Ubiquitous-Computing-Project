40daed7b597f837ee8a909570939ea4e5df4b7ab
Aplace <aplace030927@gmail.com>
2024-11-15 12:51:05 +0900

    feat(haptics): add basic haptics and UI feedback - currently update UI without multithreading. SHOULD be modified.

--- Projects/app/src/main/java/com/csed433project/hapticfitness/ui/back_stretch/BackStretchFragment.kt
+++ Projects/app/src/main/java/com/csed433project/hapticfitness/ui/back_stretch/BackStretchFragment.kt
@@ -1,10 +1,18 @@
 package com.csed433project.hapticfitness.ui.back_stretch
 
+import android.content.Context
 import android.os.Bundle
+import android.os.CombinedVibration
+import android.os.VibrationEffect
+import android.os.VibratorManager
+import android.view.HapticFeedbackConstants
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.view.HapticFeedbackConstantsCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.csed433project.hapticfitness.databinding.BackstretchBinding
@@ -26,10 +34,41 @@ class BackStretchFragment : Fragment() {
             ViewModelProvider(this).get(BackStretchViewModel::class.java)
 
         _binding = BackstretchBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        //val textView: TextView = binding.
-        //backStretchViewModel.text.observe(viewLifecycleOwner) {
+        val root: View = binding.root
+        var counter: Int = 0
+
+        // Will be used for more stronger haptic feedbacks or custom-defined haptic feedbacks.
+        // val vibrator = context?.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+        val progressBar: ProgressBar = binding.progressBar
+
+        progressBar.setOnTouchListener { _ : View, event: MotionEvent ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    counter = 0
+                    progressBar.setProgress(counter, true)
+                    view?.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_PRESS) ?: false
+                }
+                MotionEvent.ACTION_UP -> {
+                    view?.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY) ?: false
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    counter += 1
+                    counter %= 360
+                    progressBar.setProgress(counter, true)
+                    view?.performHapticFeedback(HapticFeedbackConstants.CLOCK_TICK) ?: false
+                }
+                else -> {
+                    view?.performHapticFeedback(HapticFeedbackConstants.NO_HAPTICS) ?: false
+                }
+            }
+        }
+
+
+        backStretchViewModel.text.observe(viewLifecycleOwner) {
+            progressBar.setProgress(counter, true)
+        }
+        //backStretchViewModel.pro.observe(viewLifecycleOwner) {
         //    textView.text = it
         //}
         return root
