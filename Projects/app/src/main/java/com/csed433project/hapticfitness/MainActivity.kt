5550c7a8371d8aa5dc5a6b05450da5d48e0d3a76
Temp_Name <hcg082743@gmail.com>
2024-12-15 09:26:11 +0900

    feat: UI Fixed

--- Projects/app/src/main/java/com/csed433project/hapticfitness/MainActivity.kt
+++ Projects/app/src/main/java/com/csed433project/hapticfitness/MainActivity.kt
@@ -1,26 +1,21 @@
 package com.csed433project.hapticfitness
 
 import android.os.Bundle
-import android.widget.Button
 import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
 import com.csed433project.hapticfitness.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-
-    private lateinit var backStretchButton: Button
-    private lateinit var sideStretchButton: Button
-    private lateinit var squatButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,35 +25,17 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
 
-        backStretchButton = findViewById(R.id.backStretchButton)
-        sideStretchButton = findViewById(R.id.sideStretchButton)
-        squatButton = findViewById(R.id.squatButton)
-
-        backStretchButton.setOnClickListener { navController.navigate(R.id.action_nav_home_to_nav_back_stretch) }
-        sideStretchButton.setOnClickListener { navController.navigate(R.id.action_nav_home_to_nav_side_stretch) }
-        squatButton.setOnClickListener { navController.navigate(R.id.action_nav_home_to_nav_squat) }
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home,
-                R.id.nav_back_stretch,
-                R.id.nav_squat,
-                R.id.nav_rhythm_fitness
-            ), drawerLayout
-        )
+            setOf(R.id.nav_home), drawerLayout)
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onSupportNavigateUp(): Boolean {
